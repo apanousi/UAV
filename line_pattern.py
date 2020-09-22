a049e0ce328576a5b837ba419c0ee2f2c0d18c7b
Ashley Panousis <apanousi@nd.edu>
2020-09-21 21:07:52 -0400

    Added index

--- line_pattern.py
+++ line_pattern.py
@@ -18,9 +18,9 @@ def line(copters):
     even_up(copters)
     return_center_all(copters)
 
-    move_down(copters[3], 2)
-    move_up(copters[0], 10)
-    move_up(copters[4], 10)
+    move_down(copters[3], 2, 3)
+    move_up(copters[0], 10, 0)
+    move_up(copters[4], 10, 4)
     form_increasing_line(copters)
 
     return_center_all(copters)
@@ -28,52 +28,54 @@ def line(copters):
     return 0;
 
 def form_increasing_line(copters):
-    move_up(copters[1], 1)
-    move_up(copters[2], 2)
-    move_up(copters[3], 3)
-    move_up(copters[4], 4)
+    move_up(copters[1], 1, 1)
+    move_up(copters[2], 2, 2)
+    move_up(copters[3], 3, 3)
+    move_up(copters[4], 4, 4)
 
 def even_up(copters):
-    move_up(copters[0], 5)
-    move_up(copters[2], 5)
-    move_up(copters[4], 5)
+    move_up(copters[0], 5, 0)
+    move_up(copters[2], 5, 2)
+    move_up(copters[4], 5, 4)
 
 def even_down(copters):
-    move_down(copters[0],5)
-    move_down(copters[2],5)
-    move_down(copters[4],5)
+    move_down(copters[0],5,0)
+    move_down(copters[2],5,2)
+    move_down(copters[4],5,4)
 
-def return_center(copter):
+def return_center(copter, index):
     lat = copter.location.global_relative_frame.lat
     lon = copter.location.global_relative_frame.lon
     alt = 10
     point = LocationGlobalRelative(lat, lon, alt)
     copter.simple_goto(point)
-    print_point(point)
+    print_point(point, index)
     time.sleep(6)
 
 def return_center_all(copters):
+    i = 0
     for c in copters:
-        return_center(c)
+        return_center(c, i)
+        i=i+1
 
-def move_up(copter, change):
+def move_up(copter, change, index):
     lat = copter.location.global_relative_frame.lat
     lon = copter.location.global_relative_frame.lon
     alt = copter.location.global_relative_frame.alt
     point = LocationGlobalRelative(lat, lon, alt + change)
-    print_point(point)
+    print_point(point, index)
     copter.simple_goto(point)
     time.sleep(1*change)
 
-def move_down(copter, change):
+def move_down(copter, change, index):
     lat = copter.location.global_relative_frame.lat
     lon = copter.location.global_relative_frame.lon
     alt = copter.location.global_relative_frame.alt
     point = LocationGlobalRelative(lat, lon, alt - change)
-    print_point(point)
+    print_point(point, index)
     copter.simple_goto(point)
     time.sleep(1*change)
 
-def print_point(point):
-    print("New point is ")
+def print_point(point, index):
+    print("Copter " + str(index) + " is at:")
     print(point)
