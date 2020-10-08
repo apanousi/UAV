1ead70c2cdb21b452a1ada342fbe8518ca3072f9
milktea292 <milktea292@user.noreply.github.com>
2020-10-08 01:24:01 -0400

    update for the explainations

--- generate_vc.py
+++ generate_vc.py
@@ -11,16 +11,16 @@ def generate_vc(x1,y1,x2,y2,tx1,ty1,tx2,ty2):
     c1=0
     c2=0
 
-    ## check if their slope are the same or not, if no, that means they might have a crossing point
+    ## check if their slopes are the same or not, if no, that means they might have a crossing point
     if s1 != s2:
 
-        ## computing the crossing point 
+        ## computing the (virual) crossing point 
         c1 = y1-s1*x1
         c2 = y2-s2*x2
         vcx = (c2-c1)/(s1-s2)
         vcy = (c1*s2-c2*s1)/(s2-s1) 
 
-        ## check if the crossing point is in the line segment or not (first make sure the boundaries)
+        ## check if this crossing point is in the line segment or not (first we need to make sure the boundaries)
         right1=0
         left1=0
         right2=0
@@ -40,12 +40,13 @@ def generate_vc(x1,y1,x2,y2,tx1,ty1,tx2,ty2):
 
         if x2 < tx2:  ###    x2 <---> tx2
             right2 = tx2
-            left2
+            left2 = x2
 
-        ## if the crossing point is in the segments, make the crossing point as VC
-        if left1 < vcx and vcx < right1 and left2 < vcx and vcx < right2:
+        ## if the crossing point is in the segments, make the crossing point as VC and return it
+        if left1 <= vcx and vcx <= right1 and left2 <= vcx and vcx <= right2:
             print("two segmented lines have a crossing point")
             return (vcx, vcy)
+        ## the crossing point is not in the center, that means we dont need collision avoidance in this case
         else:
             print("No crossing point")
 
@@ -57,4 +58,5 @@ def generate_vc(x1,y1,x2,y2,tx1,ty1,tx2,ty2):
         #    do nothing since they wont crash
         # else:
         # return midpoint of two drones ( (x1+x2)/2, (y1+y2)/2 )
+print(generate_vc(0,0,2,3,3,3,4,3))
 print(generate_vc(0,0,2,-3,1,1,4,-3))
